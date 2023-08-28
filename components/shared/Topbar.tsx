d74b3edd809dec9d9f25d6265d08c3144d36bc7e
IanEKu <ianeddyk@gmail.com>
2023-08-28 16:36:16 +0700

    Moving device

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignedIn, SignOutButton, OrganizationSwitcher } from '@clerk/nextjs'
+import { dark } from "@clerk/themes"
 
 export default function Topbar() {
     return (
@@ -36,6 +37,7 @@ export default function Topbar() {
 
           <OrganizationSwitcher
             appearance={{
+              baseTheme: dark,
               elements: {
                 organizationSwitcherTrigger: 'py-2 px-4'
               }
